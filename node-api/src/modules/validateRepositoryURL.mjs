26fce26c6a2b5ab755a61f7f37ea7bd092f211a6
Edgar Muniz Berlinck <edgar.vv@gmail.com>
2020-08-01 06:50:23 -0300

    Feat: Created a module to validate provided url

--- node-api/src/modules/validateRepositoryURL.mjs
+++ node-api/src/modules/validateRepositoryURL.mjs
@@ -1,3 +1,25 @@
+import URL from 'url'
+
 export default function (repositoryUrl) {
-    return Promise.resolve()
+  try {
+    const url = URL.parse(repositoryUrl)
+    
+    if (url.host !== 'github.com') {
+      return null
+    }
+
+    if (!['http:', 'https:'].includes(url.protocol)) {
+      return null
+    }
+
+    if (url.pathname === '/') {
+      return null
+    }
+    
+    console.log(url)
+    return url
+  } catch {
+    return null
+  }
+    
 }
